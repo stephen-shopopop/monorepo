d80314ca77a2cd33fcd0b557ce93941d54155453
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2023-04-15 19:06:54 +0200

    Update index.ts

--- packages/hello/library/index.ts
+++ packages/hello/library/index.ts
@@ -1,6 +1,7 @@
 import pino from 'pino'
 
 // ⛔️ Configuration applicative couplé dans la librairie
+// ⛔️ Typage: aucun typage strict protegant le passage d’arguments à la dependance
 const logger = pino({ level: process.env['LEVEL'] ?? 'info' })
 
 export default logger
