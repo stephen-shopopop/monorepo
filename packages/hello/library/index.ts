b6ac5feeafbb49b207ef294f5546ad210e95368e
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2023-04-15 15:55:07 +0200

    feat(bundle): add tsconfig more strict

--- packages/hello/library/index.ts
+++ packages/hello/library/index.ts
@@ -1,6 +1,6 @@
 import pino from 'pino'
 
 // ⛔️ Configuration applicative couplé dans la librairie
-const logger = pino({ level: process.env.LEVEL ?? 'info' })
+const logger = pino({ level: process.env['LEVEL'] ?? 'info' })
 
 export default logger
