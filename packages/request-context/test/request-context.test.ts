cd43d1d07803b8ea255f019ef03e444ef326327d
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2023-05-06 10:48:48 +0200

    feat(request-context): Init library request-context

---
+++ packages/request-context/test/request-context.test.ts
@@ -0,0 +1,52 @@
+import sinon from 'sinon'
+import { context } from '../index'
+
+describe('request-context', () => {
+  test('When instantiating a new context with initial context, then should return the initial context', () => {
+    // Arrange
+    const getCurrentContext = sinon.spy(() => context().getStore())
+
+    const initContext = { requestId: 'a' }
+
+    // Act
+    context().run({ ...initContext }, getCurrentContext)
+
+    // Assert
+    expect({
+      getStoreSpyReturnValue: getCurrentContext.returnValues[0]
+    }).toEqual({
+      getStoreSpyReturnValue: initContext
+    })
+  })
+
+  test('When instantiating a new context object and add properties, then return keys/values of store', () => {
+    // Arrange
+    const getCurrentContext = sinon.spy(() => context().getStore())
+
+    // Act
+    context().run({}, () => {
+      context().getStore().message = 'Yala'
+      context().getStore().label = 'koa'
+
+      getCurrentContext()
+    })
+
+    // Assert
+    expect({
+      getStoreSpyReturnValue: getCurrentContext.returnValues[0]
+    }).toEqual({
+      getStoreSpyReturnValue: {
+        label: 'koa',
+        message: 'Yala'
+      }
+    })
+  })
+
+  test('When no instantiating a new context, then return undefined', () => {
+    // Act
+    const getStore = context().getStore()
+
+    // Assert
+    expect(getStore).toEqual(undefined)
+  })
+})
