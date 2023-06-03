297579e7a86c76d1793f1716589bf778dee38f46
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2023-06-03 18:46:23 +0200

    Update index.ts

--- packages/jwt-verifier/index.ts
+++ packages/jwt-verifier/index.ts
@@ -1,2 +1,3 @@
 export { jwtVerifierExpressMiddleware } from './library/express/middleware'
 export { jwtVerifierKoaMiddleware } from './library/koa/middleware'
+export { userClaimsJwtVerifier } from './library/common'
